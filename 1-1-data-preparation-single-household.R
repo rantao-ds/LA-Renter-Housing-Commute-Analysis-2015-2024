da2bd90442c868f1bc2758c93cfe1509d42cf8ed
Ran Tao <281403947+ranran-tao@users.noreply.github.com>
2026-07-24 15:27:17 +0800

    Update 1-1-data-preparation-single-household.R

--- 1-1-data-preparation-single-household.R
+++ 1-1-data-preparation-single-household.R
@@ -166,4 +166,173 @@ select(-all_of(drop_sh))
 
 saveRDS(sh, "data/sh_final_cleaned.rds")
 
-Part 2 : 
+Part 2 : relabeling characters 
+
+#vehicle
+sh <- sh %>%
+  mutate(VEHICLES = case_when(
+    VEHICLES == 9 ~ "no_available", 
+    TRUE ~ as.character(VEHICLES)   
+  ))
+
+# UNITSSTR
+sh <- sh %>%
+  mutate(
+    UNITSSTR = case_when(
+      UNITSSTR == 3  ~ "1_fh_detached",
+      UNITSSTR == 4  ~ "1_fh_attached",
+      UNITSSTR == 5  ~ "2_fb",
+      UNITSSTR == 6  ~ "3-4_fb",
+      UNITSSTR == 7  ~ "5-9_fb",
+      UNITSSTR == 8  ~ "10-19_fb",
+      UNITSSTR == 9  ~ "20-49_fb",
+      UNITSSTR == 10 ~ "50_plus_fb"
+    ),
+    UNITSSTR = factor(UNITSSTR, levels = c(
+      "1_fh_detached", "1_fh_attached", "2_fb", "3-4_fb", 
+      "5-9_fb", "10-19_fb", "20-49_fb", "50_plus_fb"
+    ))
+  )
+
+# BEDROOMS
+sh <- sh %>%
+ mutate(BEDROOMS = case_when(
+  BEDROOMS == 1 ~ "no_bedrooms" ,
+  BEDROOMS == 2 ~ "1_bedrooms",
+  BEDROOMS == 3 ~ "2_bedrooms",
+  BEDROOMS == 4 ~ "3_bedrooms",
+  BEDROOMS == 5 ~ "4_bedrooms",
+  BEDROOMS == 6 ~ "5_bedrooms",
+  BEDROOMS == 8 ~ "7_bedrooms"
+ ),
+ BEDROOMS = factor(BEDROOMS, levels = c(
+     "no_bedrooms", "1_bedrooms", "2_bedrooms", "3_bedrooms", 
+     "4_bedrooms", "5_bedrooms", "7_bedrooms"
+   ))
+ )
+
+#RELATED
+sh <- sh %>%
+  mutate(RELATED = case_when(
+    RELATED == 101 ~ "head"
+  ))
+
+#SEX
+sh <- sh %>%
+mutate(SEX = case_when(
+  SEX == 1 ~ "male",
+  SEX == 2 ~ "female")
+)
+
+sh <- sh %>%
+    mutate(SEX = factor(SEX, levels = c("male", "female")))
+
+#RACE
+sh <- sh %>%
+  mutate(
+    RACE = case_when(
+      RACE == 1 ~ "white", 
+      RACE == 2 ~ "black_african_american",
+      RACE == 3 ~ "american_indian_alaska_native",
+      RACE == 4 ~ "chinese",
+      RACE == 5 ~ "japanese",
+      RACE == 6 ~ "other_asian_pacific_islander",
+      RACE == 7 ~ "other_race",
+      RACE == 8 ~ "two_major_races",
+      RACE == 9 ~ "three_plus_major_races"
+    ),
+    
+    RACE = factor(RACE, levels = c(
+      "white", "black_african_american", "american_indian_alaska_native",
+      "chinese", "japanese", "other_asian_pacific_islander", 
+      "other_race", "two_major_races", "three_plus_major_races"
+    ))
+  )
+
+#HISPAN
+sh <- sh %>%
+  mutate(
+    HISPAN = case_when(
+      HISPAN == 0 ~ "not_hispanic",
+      HISPAN == 1 ~ "mexican",
+      HISPAN == 2 ~ "puerto_rican",
+      HISPAN == 3 ~ "cuban",
+      HISPAN == 4 ~ "other_hispanic"
+    ),
+    HISPAN = factor(HISPAN, levels = c(
+      "not_hispanic", "mexican", "puerto_rican", "cuban", "other_hispanic"
+    ))
+  )
+
+#EDUCD
+sh <- sh %>%
+  mutate(
+    EDUCD = case_when(
+      EDUCD %in% c(2, 11, 12, 14, 15, 16, 17, 22, 23, 25, 26, 30, 40, 50, 61) ~ "less_than_hs",
+      EDUCD %in% c(63, 64) ~ "hs_or_ged",
+      EDUCD %in% c(65, 71, 81) ~ "some_college_associate",
+      EDUCD == 101 ~ "bachelors",
+      EDUCD %in% c(114, 115, 116) ~ "advanced_degree"
+    ),
+    EDUCD = factor(EDUCD, levels = c(
+     "less_than_hs", "hs_or_ged", "some_college_associate", "bachelors", "advanced_degree"
+    ))
+  )
+
+#TRANWORK
+sh <- sh %>%
+  mutate(
+    TRANWORK = case_when(
+      TRANWORK == 10 ~ "auto_truck_van",
+      TRANWORK == 20 ~ "motorcycle",
+      TRANWORK == 31 ~ "bus",
+      TRANWORK == 34 ~ "light_rail_streetcar",
+      TRANWORK == 36 ~ "subway_elevated",
+      TRANWORK == 37 ~ "commuter_train",
+      TRANWORK == 38 ~ "taxi_ride_hail",
+      TRANWORK == 39 ~ "ferryboat",
+      TRANWORK == 50 ~ "bicycle",
+      TRANWORK == 60 ~ "walked",
+      TRANWORK == 70 ~ "other",
+      TRANWORK == 80 ~ "wfh"
+    ),
+    TRANWORK= factor(TRANWORK, levels = c(
+      "auto_truck_van", "motorcycle", "bus", "light_rail_streetcar", 
+      "subway_elevated", "commuter_train", "taxi_ride_hail", "ferryboat", 
+      "bicycle", "walked", "other", "wfh"
+    ))
+  )
+
+# creating a new variable to label the trans_mode into four groups
+sh <- sh %>%
+mutate(
+    transit_group = case_when(
+       transit_mode %in% c("auto_truck_van", "motorcycle") ~ "private_auto",
+       transit_mode %in% c("bus", "subway_elevated", "commuter_train", "light_rail_streetcar", "ferryboat") ~ "public_transit",
+       transit_mode %in% c("walked", "bicycle") ~ "active_transit",
+       transit_mode == "wfh" ~ "wfh",
+       transit_mode %in% c("taxi_ride_hail", "other") ~ "other_transit"
+    )
+  )
+
+#CARPOOL
+sh <- sh %>%
+  mutate(
+    # Step 1: Map your 3 active codes to clean text labels
+    CARPOOL = case_when(
+      CARPOOL == 0 ~ "not_applicable",
+      CARPOOL == 1 ~ "drives_alone",
+      CARPOOL == 2 ~ "carpools"
+    ),
+    
+    # Step 2: Convert to factor (Setting "drives_alone" as your baseline)
+    CARPOOL = factor(CARPOOL, levels = c(
+     "not_applicable", "drives_alone", "carpools"
+    ))
+  )
+
+# creating a new variable to label the type of household 
+  sh <- sh %>% 
+     mutate(hh_group = "single")
+
+saveRDS(sh, "data/sh_eda_ready.rds")
